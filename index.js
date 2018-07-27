b44469dd91709d689712bb540c56824b516e1311
Matt Farmer <mrfarmer777@gmail.com>
2018-07-27 05:01:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Code your solution in this file!
+function logDriverNames(drvs){
+  drvs.forEach(function(drv){
+    console.log(drv.name);
+  });
+}
+
+function logDriversByHometown(drvs,location){
+  drvs.forEach(function(drv){
+    if(drv.hometown===location){
+      console.log(drv.name);
+    }
+  });
+}
+
+function driversByRevenue(drvs){
+  myDrvs=drvs.slice();
+  return myDrvs.sort(function(a,b){
+    //Subtract second driver's revenue from first's. Two should switch if number is negative.
+    return a.revenue-b.revenue;
+  });
+}
+
+function driversByName(drvs){
+  myDrvs=drvs.slice();
+  return myDrvs.sort(function(a,b){
+    return a.name.localeCompare(b.name);
+  });
+}
+
+function totalRevenue(drvs){
+  console.log(drvs[0].revenue);
+  return drvs.reduce(getTotal,0);
+}
+
+function getTotal(total,el){
+  return total+el.revenue;
+}
+
+function averageRevenue(drvs){
+  return totalRevenue(drvs)/drvs.length;
+}
+
